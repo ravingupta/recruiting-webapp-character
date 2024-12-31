5a5d5345626b42f6a1a8c11e3e0709fd695213bf
Ravin Gupta <ravingupta129@gmail.com>
2024-12-31 13:36:13 -0800

    added intelligent modifier

--- src/app/components/attribute.tsx
+++ src/app/components/attribute.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 
 import { AttributeProps } from '../types';
 
@@ -18,15 +18,21 @@ const AttributeComponent = React.memo(({attributeName, val, maxVal, callback}: A
     return;
   }
 
+  const modifier = useMemo(() => {
+    return Math.floor((val - 10) / 2);
+  }, [val]);
+
   return (
     <div className="p-4 mb-2 bg-gray-100 flex justify-between items-center">
       <h4 className="text-lg font-semibold">
         {attributeName} - {val}
+        <br />
+        <span className='text-sm text-gray-500'>Modifier: { modifier }</span>
       </h4>
       <div>
-          <button className='btn px-2 py-1 bg-blue-500 text-white' onClick={() => updateValue('add')}>+</button>
-          <button className='btn px-2 py-1 bg-red-500 text-white ml-2' onClick={() => updateValue('sub')}>-</button>
-        </div>
+        <button className='btn px-2 py-1 bg-blue-500 text-white' onClick={() => updateValue('add')}>+</button>
+        <button className='btn px-2 py-1 bg-red-500 text-white ml-2' onClick={() => updateValue('sub')}>-</button>
+      </div>
     </div>
   )
 });
