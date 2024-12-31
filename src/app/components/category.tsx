f6eae9b2a3ac433ed1d581a05c23fe82577484fd
Ravin Gupta <ravingupta129@gmail.com>
2024-12-31 13:15:52 -0800

    categories expanded

--- src/app/components/category.tsx
+++ src/app/components/category.tsx
@@ -5,6 +5,7 @@ import { CLASS_LIST } from '../consts';
 
 const CategoryComponent = React.memo(({ categoryName, currentAttributes }: CategoryProps) => {
   const [active, setActive] = React.useState(false);
+  const [expanded, setExpanded] = React.useState(false);
 
   useEffect(() => {
     let tempActive = true;
@@ -18,13 +19,27 @@ const CategoryComponent = React.memo(({ categoryName, currentAttributes }: Categ
   
 
   return (
-  <div className="p-4 mb-2 bg-gray-100 flex justify-between items-center">
-    <h4 className="text-lg font-semibold">
-      {categoryName} 
-    </h4>
-    <span className={`ml-2 px-2 py-1 rounded text-sm ${active ? 'bg-green-500 text-white' : 'bg-gray-300'}`}>
-      {active ? 'Active' : 'Inactive'}
-    </span>
+  <div className="p-4 mb-2 bg-gray-100" onClick={() => setExpanded(!expanded)}>
+    <div className='flex justify-between items-center'>
+      <h4 className="text-lg font-semibold">
+        {categoryName} 
+      </h4>
+      <span className={`ml-2 px-2 py-1 rounded text-sm ${active ? 'bg-green-500 text-white' : 'bg-gray-300'}`}>
+        {active ? 'Active' : 'Inactive'}
+      </span>
+    </div>
+
+    {expanded && (
+      <div className="mt-4">
+        <h5 className="text-md font-semibold">Minimum required</h5>
+        {Object.keys(CLASS_LIST[categoryName]).map((attribute) => (
+          <div key={attribute} className="flex justify-between items-center p-4">
+            <h5 className="text-md font-semibold">{attribute}</h5>
+            <span>{CLASS_LIST[categoryName][attribute]}</span>
+          </div>
+        ))}
+      </div>
+    )}
   </div>
 )});
 
